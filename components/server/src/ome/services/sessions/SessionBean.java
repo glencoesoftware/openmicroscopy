5b9842a71359f979343fd98a328ce75e6b6162ad
Kevin Kozlowski <kevin@glencoesoftware.com>
2019-12-03 14:06:59 -0700

    Expand user session creation abilities
    A system wide, configurable maximum time to idle and time to live is now
    in place.  Regular users are allowed to create new sessions with
    timeouts up to these limits.  The previous limits were 10x the default
    time to idle and time to live and those are now the default configured
    values.  These new properties are:
    
     * `omero.sessions.max_user_time_to_idle`
     * `omero.sessions.max_user_time_to_live`
    
    Furthermore, it is now possible to pass an "omero.agent" as part of call
    context to set the user agent on sessions created using the session
    service.  For example:
    
      createUserSession(0, 600000, 'test', {'omero.agent': 'my agent'})
    
    Finally, the following properties have been exposed to the via the
    configuration service so that a user may make predictable and informed
    choices during session creation:
    
     * `omero.sessions.timeout`
     * `omero.sessions.maximum`
     * `omero.sessions.max_user_time_to_idle`
     * `omero.sessions.max_user_time_to_live`
    
    Integration tests have been added to cover the various modifications.

--- components/server/src/ome/services/sessions/SessionBean.java
+++ components/server/src/ome/services/sessions/SessionBean.java
@@ -89,13 +89,14 @@ public class SessionBean implements ISession {
             currentSession = null;
         }
 
+        final String agent = cd.getContext().get("omero.agent");
         try {
             final Principal principal = principal(defaultGroup, user);
             Future<Session> future = ex.submit(new Callable<Session>(){
                 public Session call() throws Exception {
                     final CreationRequest req = new CreationRequest();
                     req.principal = principal;
-                    req.agent = "createSession";
+                    req.agent = agent == null ? "createSession" : agent;
                     if (currentSession != null) {
                         final Experimenter sudoer = currentSession.getSudoer();
                         if (sudoer != null) {
@@ -111,7 +112,6 @@ public class SessionBean implements ISession {
         } catch (Exception e) {
             throw creationExceptionHandler(e);
         }
-
     }
 
     @RolesAllowed("user" /* group owner */)
@@ -148,12 +148,13 @@ public class SessionBean implements ISession {
             groupsLed = context.getLeaderOfGroupsList();
         }
 
+        final String agent = cd.getContext().get("omero.agent");
         try {
             Future<Session> future = ex.submit(new Callable<Session>(){
                 public Session call() throws Exception {
                     SessionManager.CreationRequest req = new SessionManager.CreationRequest();
                     req.principal = principal;
-                    req.agent = "OMERO.sudo";
+                    req.agent = agent == null ? "OMERO.sudo" : agent;
                     req.groupsLed = groupsLed;
                     req.timeToIdle = timeToIdleMilliseconds;
                     req.timeToLive = timeToLiveMilliseconds;
@@ -176,10 +177,13 @@ public class SessionBean implements ISession {
     @RolesAllowed( { "user", "guest" })
     public Session createSession(@NotNull Principal principal,
             @Hidden String credentials) {
-
         Session session = null;
         try {
-            session = mgr.createWithAgent(principal, credentials, "createSession", null);
+            String agent = cd.getContext().get("omero.agent");
+            if (agent == null) {
+                agent = "createSession";
+            }
+            session = mgr.createWithAgent(principal, credentials, agent, null);
         } catch (Exception e) {
             throw creationExceptionHandler(e);
         }
